e663ce6d3f50b5c85d333933c978be949f91ddc8
dhrisdhruv <dhrisdhruv@gmail.com>
2023-07-05 19:42:38 +0530

    v1

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:dio/dio.dart';
@@ -11,7 +12,7 @@ class HomePage extends StatefulWidget {
   final Dio dio = Dio();
   //final GetConnect connect = GetConnect();
   late List timeSeriesStamp, temperatureSeries;
-  late List<WeatherData> weatherData;
+  late List weatherSeries;
 
   Future<dynamic> getWeather() async {
 
@@ -19,10 +20,12 @@ class HomePage extends StatefulWidget {
       "https://api.open-meteo.com/v1/forecast",
       queryParameters: {"latitude": position.latitude, "longitude": position.longitude, "hourly": "temperature_2m"},
     );
-    timeSeriesStamp = response.data["hourly"]["time"].map((e)=>DateTime.parse(e.toString())).toList();
+    timeSeriesStamp = response.data["hourly"]["time"].map((e)=>DateTime.parse(e.toString()).hour.toDouble()).toList();
     temperatureSeries = response.data["hourly"]["temperature_2m"];
-    weatherData = zip([timeSeriesStamp, temperatureSeries]).toList().map((e) => WeatherData(e[0], e[1])).toList();
-    return response.data;
+    timeSeriesStamp.length = 20;
+    temperatureSeries.length = 20;
+    weatherSeries = zip([timeSeriesStamp,temperatureSeries]).toList();
+    return weatherSeries;
   }
 
   Future<Position> getLocation() async {
@@ -40,54 +43,74 @@ class HomePage extends StatefulWidget {
 class HomePageState extends State<HomePage> {
 
   @override
+  void setState(VoidCallback fn) {
+    widget.getLocation();
+    widget.getWeather();
+    // TODO: implement setState
+    super.setState(fn);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: FutureBuilder(
-          future: widget.getLocation(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if(snapshot.data == null) {
-              return CircularProgressIndicator();
-            } else {
-              return FutureBuilder(
-                future: widget.getWeather(),
-                builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  if(snapshot.data == null) {
-                    return CircularProgressIndicator();
-                  }
-                  else {
-                    return Text(snapshot.data.toString());
-                  }
-                },
-              );
-            }
-          },
+    return SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: FutureBuilder(
+              future: widget.getLocation(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if(snapshot.data == null) {
+                  return CircularProgressIndicator();
+                } else {
+                  return FutureBuilder(
+                    future: widget.getWeather(),
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if(snapshot.data == null) {
+                        return CircularProgressIndicator();
+                      }
+                      else {
+                        return ShowGraph(snapshot.data);
+                      }
+                    },
+                  );
+                }
+              },
+            ),
+          ),
         ),
-      ),
     );
+
   }
 
 }
 
-class WeatherData {
-  DateTime time;
-  double temperature;
-  WeatherData(this.time,this.temperature);
-}
-
 class ShowGraph extends StatelessWidget {
-  List<WeatherData> weatherData;
-  ShowGraph(this.weatherData);
+  final List points;
+  ShowGraph(this.points);
+
   @override
   Widget build(BuildContext context) {
-    return charts.SfCartesianChart(
-      primaryXAxis: charts.DateTimeAxis(),
-      series: <charts.ChartSeries>[
-        charts.LineSeries(
-            dataSource: weatherData,
-            xValueMapper: (WeatherData weather, _)=>weather.time.hour,
-            yValueMapper: yValueMapper)
-      ],
+    /*
+    int screenWidth = MediaQuery.of(context).size.width.toInt();
+    int screenHeight = MediaQuery.of(context).size.height.toInt();
+    int min,max;
+    if(screenWidth<screenHeight) {
+      min = screenWidth; max = screenHeight;
+    } else {
+      min = screenHeight; max = screenWidth;
+    }
+    */
+
+    return LineChart(
+      LineChartData(
+        lineTouchData: LineTouchData(enabled: false),
+        lineBarsData: [
+          LineChartBarData(
+            spots: points.map((point)=>FlSpot(point[0], point[1])).toList(),
+            isCurved: true,
+            dotData: FlDotData(show: false),
+          ),
+        ],
+      ),
     );
   }
 
